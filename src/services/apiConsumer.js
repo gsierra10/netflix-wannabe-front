aef862d03f135be32fbd8b86140fb1daa246b286
Raul <raaul9212@gmail.com>
2021-11-21 21:56:22 +0100

    Find Movie

--- src/services/apiConsumer.js
+++ src/services/apiConsumer.js
@@ -1,39 +1,39 @@
 export const APIConsumer = {
 
-loginUser: async (data) => {
-    try{
-        let result = await fetch('http://localhost:0420/user/login', {
+    loginUser: async (data) => {
+        try{
+            let result = await fetch('http://localhost:0420/user/login', {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: data
+            })
+            result = result.json()
+            localStorage.setItem('token', result)
+        } catch(data){
+            console.log(data)
+        }
+    },
+
+    registerUser: async (data) => {
+        try{
+            let result = await fetch('http://localhost:0420/user/register', {
             method: "POST",
-            headers: {'Content-Type': 'application/json'},
             body: data
-        })
-        result = result.json()
-        localStorage.setItem('token', result)
-    } catch(data){
-        console.log(data)
-    }
+            })
+        } catch (data){
+            console.log(data)
+        }
     },
 
-registerUser: async (data) => {
-    try{
-        let result = await fetch('http://localhost:0420/user/register', {
-        method: "POST",
-        body: data
-        })
-    } catch (data){
-        console.log(data)
-    }
-    },
-
-searchMovie: async (data) => {
-    try{
-        let result = await fetch('',{
-            method: "GET",
-            body: data
-        })
-    } catch(data){
-        console.log(data)
-    }
+    findMovie: async (data) => {
+        try{
+            let result = await fetch('http://localhost:0420/movie/',{
+                method: "GET",
+                body: data
+            })
+        } catch(data){
+            console.log(data)
+        }
     },
 
 }
