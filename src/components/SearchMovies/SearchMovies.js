30dbef90d939f87b59dedfcad2ae32ae572e2624
gricosm <guillermo.rico.sm@gmail.com>
2021-11-22 01:15:03 +0100

    More changes for Heroku

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -3,19 +3,22 @@ import MovieCard from '../MovieCard/MovieCard'
 
 const SearchMovies = () => {
 	const[movies, setMovies] = useState([]);
-	const[loading, setLoading] = useState(true);
-	const[error, setError] = useState(false);
+	// const[loading, setLoading] = useState(true);
+	// const[error, setError] = useState(false);
 
 	const getMovies = async() => {
 		try {
 			let res = await fetch('http://localhost:0420/movies')
 			res = await res.json()
-			setMovies(movies = res.movies)
-			setLoading(false)
+			return setMovies(movies)
+			
+			// setLoading(false)
 		} catch (error) {
-			setError(true)
-			setLoading(false)
+			console.log("errorsito")
+			// setError(true)
+			// setLoading(false)
 		}
+		console.log(movies)
 	}
 	useEffect(() =>{
 		getMovies()
@@ -27,13 +30,14 @@ const SearchMovies = () => {
             <div className="form-control">
                 <label>Nombre de la pelicula</label>
                 <input type="text" name="movie" placeholder="Busca por titulo" required/>
+				<button>Botón</button>
             </div>
         </form>
 		
 		
-			{error && <span>No podemos recuperar las peliculas</span>}
-			{loading && <h1>Buscando</h1>}
-			{movies.map((dataMovie)=>{
+			{/* {error && <span>No podemos recuperar las peliculas</span>} */}
+			{/* {loading && <h1>Buscando</h1>} */}
+			{[movies].map((dataMovie)=>{
 				return(<MovieCard title={dataMovie.title} director={dataMovie.director} genre = {dataMovie.genre} duration = {dataMovie.duration} actors = {dataMovie.actors}/>)
 			})}
 		</>
